ca31a3d658148160be8a05c482b6ec2d6ce2640b
SyerAru <syeraru@gmail.com>
2022-10-29 07:51:41 -0600

    Remove display functions to be added separately

--- script.js
+++ script.js
@@ -27,45 +27,48 @@ const btnEqual = document.querySelector("#btnEqual");
 const buttons = document.querySelectorAll("button");
 
 let memArr = [0];
-let operator;
+let operator = 0;
 let numA = [];
 let numB = [];
 let display = [0];
+
+function add(numA, numB) {
+    return numA + numB;
+};
+
+function subtract(numA, numB) {
+    return numA - numB;
+};
+
+function multiply(numA, numB) {
+    return numA * numB;
+};
+
+function divide(numA, numB) {
+    return numA / numB;
+};
+
+function operate(operator, numA, numB) {
+    return window[operator](numA, numB);
+};
 
 buttons.forEach((button) => {
     button.onclick = () => {
 
         if (button.classList.contains("number")) {
+
+            //Zero is default unless another number is entered 
             if (memArr == 0 && numA.length < 1) { //if you don't want to use 0 as numA
                 memArr = [];
+
             };
 
+            //Entering decimal vs number
             if (button.value == "." && !memArr.includes(".")) { //using decimal
                 memArr.push(button.value);
-                console.log("decimal", button.value)
 
             } else if (button.value != ".") { //entering numbers into memArr to be added to numA or numB later
                 memArr.push(Number(button.value));
-                console.log("memArr", memArr)
-            };
-
-            if (typeof operator == "string") { //if numA is declared
-                display = Array.from(display);
-
-                display.push(button.textContent);
-                console.log("display a+b", display)
-
-                calcText.textContent = display.join("");
-                console.log("calcText a+b", calcText.textContent)
-
-                //decimal still appears multiple times in b, but not in a
-
-            } else { //if numA and numB are not declared
-                display = memArr;
-                console.log("display a", display)
-
-                calcText.textContent = display.join("");
-                console.log("calcText a", calcText.textContent)
             };
 
         } else if (button.classList.contains("operator") && memArr.length > 0) {
@@ -73,26 +76,10 @@ buttons.forEach((button) => {
             if (typeof numA == "number" && typeof operator == "string") {
                 
                 numB = Number(memArr.join(""));
-                console.log("b", numB)
 
                 memArr = operate(operator, numA, numB);
-                console.log(memArr)
-                display = memArr;
-                calcText.textContent = display;
-
-                // if () {
-                //     display = Array.from(display);
-
-                //     display.push(button.textContent);
-                //     console.log("display op", display)
-    
-                //     calcText.textContent = display.join("");
-                //     console.log("calcText op", calcText.textContent)
-                // }
                 
-
                 numA = memArr;
-                console.log(numA)
 
                 memArr = [];
 
@@ -102,18 +89,10 @@ buttons.forEach((button) => {
 
             } else {
                 numA = Number(memArr.join(""));
-                console.log("a", numA)
 
                 operator = button.id;
                 memArr = [];
 
-                display = Array.from(display);
-
-                display.push(button.textContent);
-                console.log("display op", display)
-
-                calcText.textContent = display.join("");
-                console.log("calcText op", calcText.textContent)
             };
 
         } else if (button.classList.contains("equal") && typeof operator == "string" && memArr.length > 0) {
@@ -121,24 +100,21 @@ buttons.forEach((button) => {
 
             if (operator == "divide" && numB == 0) { //dividing by zero
                 memArr = "Uh Oh"
-                display = memArr;
-                calcText.textContent = display;
 
                 operator = 0;
 
             } else { //regular operations
                 memArr = operate(operator, numA, numB);
                 memArr = [Number(+memArr.toFixed(2))];
-                display = memArr;
-                calcText.textContent = display;
 
                 operator = 0;
                 numA = memArr;
                 numB = [];
 
-                console.log("result memArr", memArr)
             };
         };
+        console.log(memArr);
+        console.log(operator);
     };
 });
 
@@ -159,24 +135,4 @@ btnClear.onclick = () => {
     };
 
     calcText.textContent = display.join("");
-};
-
-function add(numA, numB) {
-    return numA + numB;
-};
-
-function subtract(numA, numB) {
-    return numA - numB;
-};
-
-function multiply(numA, numB) {
-    return numA * numB;
-};
-
-function divide(numA, numB) {
-    return numA / numB;
-};
-
-function operate(operator, numA, numB) {
-    return window[operator](numA, numB);
 };
--- test.js
+++ test.js
@@ -10,17 +10,19 @@ buttons.forEach((button) => {
                 memArr.push(button.value);
                 console.log("decimal", button.value)
 
-            } else if (button.value != ".") {
+            } else if (button.value != ".") { //entering numbers into memArr to be added to numA or numB later
                 memArr.push(Number(button.value));
-                console.log("memarr", memArr)
+                console.log("memArr", memArr)
             };
 
             if (typeof operator == "string") { //if numA is declared
                 display = Array.from(display);
+
                 display.push(button.textContent);
-                console.log("display b", display)
+                console.log("display a+b", display)
+
                 calcText.textContent = display.join("");
-                console.log("calcText b", calcText.textContent)
+                console.log("calcText a+b", calcText.textContent)
 
                 //decimal still appears multiple times in b, but not in a
 
@@ -33,15 +35,52 @@ buttons.forEach((button) => {
             };
 
         } else if (button.classList.contains("operator") && memArr.length > 0) {
-            operator = button.id;
-            numA = Number(memArr.join(""));
-            memArr = [];
 
-            display = Array.from(display);
-            display.push(button.textContent);
-            console.log("display op", display)
-            calcText.textContent = display.join("");
-            console.log("calcText op", calcText.textContent)
+            if (typeof numA == "number" && typeof operator == "string") {
+                
+                numB = Number(memArr.join(""));
+                console.log("b", numB)
+
+                memArr = operate(operator, numA, numB);
+                console.log(memArr)
+                display = memArr;
+                calcText.textContent = display;
+
+                // if () {
+                //     display = Array.from(display);
+
+                //     display.push(button.textContent);
+                //     console.log("display op", display)
+    
+                //     calcText.textContent = display.join("");
+                //     console.log("calcText op", calcText.textContent)
+                // }
+                
+
+                numA = memArr;
+                console.log(numA)
+
+                memArr = [];
+
+                operator = button.id;
+
+                numB = [];
+
+            } else {
+                numA = Number(memArr.join(""));
+                console.log("a", numA)
+
+                operator = button.id;
+                memArr = [];
+
+                display = Array.from(display);
+
+                display.push(button.textContent);
+                console.log("display op", display)
+
+                calcText.textContent = display.join("");
+                console.log("calcText op", calcText.textContent)
+            };
 
         } else if (button.classList.contains("equal") && typeof operator == "string" && memArr.length > 0) {
             numB = Number(memArr.join(""));
@@ -63,7 +102,7 @@ buttons.forEach((button) => {
                 numA = memArr;
                 numB = [];
 
-                console.log("result memarr", memArr)
+                console.log("result memArr", memArr)
             };
         };
     };
