5d6bf045988198eed7cf95fa670af9425d7d797c
SyerAru <syeraru@gmail.com>
2022-11-09 18:43:21 -0700

    Remove redundant lines

--- script.js
+++ script.js
@@ -1,29 +1,6 @@
 const calcText = document.querySelector("#calcText");
-
 const btnClear = document.querySelector("#btnClear");
 const btnAllClear = document.querySelector("#btnAllClear");
-const btnInteger = document.querySelector("#btnInteger");
-const btnDivide = document.querySelector("#btnDivide");
-
-const btn7 = document.querySelector("#btn7");
-const btn8 = document.querySelector("#btn8");
-const btn9 = document.querySelector("#btn9");
-const btnMultiply = document.querySelector("#btnMultiply");
-
-const btn4 = document.querySelector("#btn4");
-const btn5 = document.querySelector("#btn5");
-const btn6 = document.querySelector("#btn6");
-const btnSubtract = document.querySelector("#btnSubtract");
-
-const btn1 = document.querySelector("#btn1");
-const btn2 = document.querySelector("#btn2");
-const btn3 = document.querySelector("#btn3");
-const btnAdd = document.querySelector("#btnAdd");
-
-const btn0 = document.querySelector("#btn0");
-const btnDecimal = document.querySelector("#btnDecimal");
-const btnEqual = document.querySelector("#btnEqual");
-
 const buttons = document.querySelectorAll("button");
 
 let memArr = [0];
